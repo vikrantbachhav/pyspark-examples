843678d2c7f37fe7792768c5110a03b61d9b609f
vikrantbachhav <appsvikrant@gmail.com>
2023-11-04 23:25:14 -0700

    (some message stating you added .idea to ignored entries)

---
+++ pyspark-local-executors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 22:42:50 2019
+
+@author: prabha
+"""
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.functions import to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType,DateType
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+sc = spark._jsc.sc()
+n_workers = len([executor.host() for executor in sc.statusTracker().getExecutorInfos()]) - 1
+print(n_workers)
